19c68ceed511b1b255f97d9f6f769a12dd94d8e0
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-15 09:32:52 -0300

    2.72

--- JAVA HERA/src/ControleBonifacacao.java
+++ JAVA HERA/src/ControleBonifacacao.java
@@ -1,28 +1,20 @@
 
 public class ControleBonifacacao {
-	
+
 	private double soma;
-	
-	
-	
-	public void registra(Funcionario f ) {
-		double boni = f.getBonificacao();
-		this.soma= this.soma + boni;
-		
+
+	public void registra(Funcionario f) {   // fora do padrão de herança , codigos repetidos 
+		double boni = f.getBonificacao(); // 3 metodos para a mesma coisa 
+		this.soma = this.soma + boni;
+
 	}
-	
-	
-	public void registra(EditorVideo ev ) {
+
+	public void registra(EditorVideo ev) {
 		double boni = ev.getBonificacao();
-		this.soma= this.soma + boni;
-		
+		this.soma = this.soma + boni;
+
 	}
-	
-	
-	
-	
-	
-	
+
 	public double getSoma() {
 		return soma;
 	}
