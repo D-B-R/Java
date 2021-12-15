ffb6bc396ad9b42e499e289bcffccaee76fc2e12
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-15 09:29:04 -0300

    2.70

--- JAVA HERA/src/ControleBonifacacao.java
+++ JAVA HERA/src/ControleBonifacacao.java
@@ -10,6 +10,19 @@ public class ControleBonifacacao {
 		this.soma= this.soma + boni;
 		
 	}
+	
+	
+	public void registra(EditorVideo ev ) {
+		double boni = ev.getBonificacao();
+		this.soma= this.soma + boni;
+		
+	}
+	
+	
+	
+	
+	
+	
 	public double getSoma() {
 		return soma;
 	}
---
+++ JAVA HERA/src/EditorVideo.java
@@ -0,0 +1,10 @@
+
+public class EditorVideo extends Funcionario {
+
+	
+
+	public double getBonificacao() {
+		return super.getBonificacao() + 100 ; // chamei metodo bonificação
+	}
+
+}
