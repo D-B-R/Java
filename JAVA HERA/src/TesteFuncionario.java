29487aca652d36dd3a6289f87d751cf0914516e0
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-15 13:30:11 -0300

    2.72

--- JAVA HERA/src/TesteFuncionario.java
+++ JAVA HERA/src/TesteFuncionario.java
@@ -3,7 +3,7 @@ public class TesteFuncionario {
 
 	public static void main(String[] args) {
 		
-		Funcionario nico = new Funcionario();
+		Funcionario nico = new Gerente();
 		nico.setNome("nico rocha");
 		nico.setCpf("222222-90");
 		nico.setSalario(2600.00);
--- JAVA HERA/src/TesteReferencias.java
+++ JAVA HERA/src/TesteReferencias.java
@@ -8,15 +8,15 @@ public class TesteReferencias {
 		g1.setNome("marcos");
 		g1.setSalario(5000.0);
 
-		Funcionario f = new Funcionario();
-		f.setSalario(2000.0);
+		Funcionario d = new Designer();
+		d.setSalario(2000.0);
 
 		EditorVideo ev = new EditorVideo();
-		f.setSalario(2500.0);
+		ev.setSalario(2500.0);
 
 		ControleBonifacacao controle = new ControleBonifacacao();
 		controle.registra(g1);
-		controle.registra(f);
+		controle.registra(d);
 		controle.registra(ev);
 
 		System.out.println(controle.getSoma());
