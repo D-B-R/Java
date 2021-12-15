d2cf82e33c1ca9bc64a557108447593643e94c1b
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-15 09:15:40 -0300

    2.69

--- JAVA HERA/src/TesteReferencias.java
+++ JAVA HERA/src/TesteReferencias.java
@@ -3,10 +3,13 @@ public class TesteReferencias {
 
 	public static void main(String[] args) {
 
-		Gerente g1 = new Gerente(); // direito --> nome da classe
+		Gerente g1 = new Gerente(); // direito --> nome da classe esquerdo - referencias
 
 		g1.setNome("marcos");
 		String nome = g1.getNome();
+		
+		g1.autentica(2222); // analisando o tipo da referencia 
+		
 
 		System.out.println(nome);
 
